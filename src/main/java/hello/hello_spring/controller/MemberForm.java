87bbd2a438988819a24f538c902840e2054f4009
chanition <chanyeong1206@gmail.com>
2025-05-21 16:29:46 +0900

    SpringStudy : ~ memberForm

---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.domain.Member;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
