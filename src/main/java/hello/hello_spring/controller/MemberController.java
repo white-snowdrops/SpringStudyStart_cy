2fa8f7ee114b8ad3078573a9dc6c4bb3f1e92fa0
chanition <chanyeong1206@gmail.com>
2025-05-24 00:21:35 +0900

    SpringStudy : ~JdbcMemberRepository

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,11 +1,14 @@
 package hello.hello_spring.controller;
 
+import org.springframework.ui.Model;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,6 +33,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberlist";
+
 
     }
 }
